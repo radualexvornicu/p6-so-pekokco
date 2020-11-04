0f627a30b3aa5f1eab53d8c931811e9bb4758d42
radualexvornicu <radualex.vornicu@gmail.com>
2020-11-04 17:43:48 +0100

    another multer

--- routes/sauces.js
+++ routes/sauces.js
@@ -11,7 +11,7 @@ router.get('/', auth, saucesCtrl.getAllSauce);
 router.post('/', auth, multer, saucesCtrl.createSauce);
 router.get('/:id', auth, saucesCtrl.getOneSauce);
 router.put('/:id', auth,  multer, saucesCtrl.modifySauce);
-router.delete('/:id', auth, saucesCtrl.deleteSauce);
+router.delete('/:id', auth, multer, saucesCtrl.deleteSauce);
 router.post("/:id/like", auth, saucesCtrl.likeSauce);
 
 module.exports = router;
